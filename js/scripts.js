29c044407727c3ea6246d5f29959442a055c5b0a
cesare <cesare.marchesi@yahoo.it>
2020-08-29 19:25:08 +0200

    added gallery and basic index layout

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//set a default slideIndex = 0
+var slideIndex = 0;
+
+//define the slideShow function:
+function slideShow() {
+  var i;
+
+  //hide the slides
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  //start the slideshow
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "grid";
+  setTimeout(slideShow, 2000); // SLIDESHOW SPEED HERE
+}
+
+//start the function slideShow
+slideShow();
